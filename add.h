6e4de94a5a09b860b6ff1d201ca3def0321e82e2
Stefano Sciacovelli <code@morpheos.it>
2023-12-03 18:58:24 +0100

    Step 1 - Green

--- add.h
+++ add.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int add(string numbers) {
-    return 0;
+    if (numbers.empty()) {
+        return 0;
+    }
+    if (numbers.find(",") != string::npos) {
+        stringstream ss(numbers);
+        int sum = 0;
+        string num;
+
+        while (getline(ss, num, ',')) {
+            sum += stoi(num);
+        }
+
+        return sum;
+    }
+    return stoi(numbers);
 }
