fc221a95959c568661037dcb64f37a650385b5d7
M04ph3u2 <116311393+M04ph3u2@users.noreply.github.com>
2023-12-03 19:50:54 +0100

    Step 3 - Refactor

--- add.h
+++ add.h
@@ -5,16 +5,12 @@
 using namespace std;
 
 int add(string numbers) {
-    if (numbers.empty()) {
-        return 0;
-    }
-    stringstream ss;
-    ss << numbers;
     int sum = 0;
+    stringstream ss(numbers);
     string num, strg;
     while (getline(ss, strg, '\n')) {
-        stringstream ss2(strg);
-        while (getline(ss2, num, ',')) {
+        stringstream lineStream(strg);
+        while (getline(lineStream, num, ',')) {
             sum += stoi(num);
         }
     }
