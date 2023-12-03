d634a0646b32e8559f30d080944c1e12639792df
Stefano Sciacovelli <code@morpheos.it>
2023-12-03 20:37:57 +0100

    Last Refactor (Step 6)

--- add.h
+++ add.h
@@ -5,31 +5,23 @@
 using namespace std;
 
 int add(string numbers) {
-    char delimeter = ',';
+    char delimiter = ',';
     if (numbers.find("//") == 0) {
-        delimeter = numbers[2];
+        delimiter = numbers[2];
         numbers = numbers.substr(4);
-    }
-    if (numbers.find("-") != string::npos) {
-        int minusPos = numbers.find("-");
-        int substringStart = minusPos + 1;
-        int newlinePos = numbers.substr(substringStart).find('\n');
-        int delimiterPos = numbers.substr(substringStart).find(delimeter);
-        if (delimiterPos != string::npos && (newlinePos == string::npos || newlinePos < delimiterPos)) {
-            throw std::runtime_error("negatives not allowed: -" + numbers.substr(substringStart, delimiterPos));
-        }
-        else {
-            throw std::runtime_error("negatives not allowed: -" + numbers.substr(substringStart, newlinePos));
-        }
     }
     int sum = 0;
     stringstream ss(numbers);
     string num, strg;
     while (getline(ss, strg, '\n')) {
         stringstream lineStream(strg);
-        while (getline(lineStream, num, delimeter)) {
-            if (stoi(num) <= 1000) {
-                sum += stoi(num);
+        while (getline(lineStream, num, delimiter)) {
+            int currentNum = stoi(num);
+            if (currentNum < 0) {
+                throw runtime_error("negatives not allowed: " + to_string(currentNum));
+            }
+            if (currentNum <= 1000) {
+                sum += currentNum;
             }
         }
     }
