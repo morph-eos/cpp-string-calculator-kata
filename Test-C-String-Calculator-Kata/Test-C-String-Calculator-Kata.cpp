2630622038151e3a3f446ffaf848cb146172a122
M04ph3u2 <116311393+M04ph3u2@users.noreply.github.com>
2023-12-03 18:40:27 +0100

    Step 1 - Red

--- Test-C-String-Calculator-Kata/Test-C-String-Calculator-Kata.cpp
+++ Test-C-String-Calculator-Kata/Test-C-String-Calculator-Kata.cpp
@@ -9,6 +9,19 @@ namespace TestCStringCalculatorKata
 	TEST_CLASS(TestCStringCalculatorKata)
 	{
 	public:
+		TEST_METHOD(TestMethodEmptyString)
+		{
+			Assert::AreEqual(0, add(""));
+		}
 
+		TEST_METHOD(TestMethodOneNumber)
+		{
+			Assert::AreEqual(1, add("1"));
+		}
+
+		TEST_METHOD(TestMethodTwoNumbers)
+		{
+			Assert::AreEqual(3, add("1,2"));
+		}
 	};
 }
