c3ada5b2b29cc8dae453ae312740ce786102252a
M04ph3u2 <116311393+M04ph3u2@users.noreply.github.com>
2023-12-03 20:06:55 +0100

    Step 4 - Green
    No need to refactor after this

--- add.h
+++ add.h
@@ -5,12 +5,17 @@
 using namespace std;
 
 int add(string numbers) {
+    char delimeter = ',';
+    if (numbers.find("//") == 0) {
+        delimeter = numbers[2];
+        numbers = numbers.substr(4);
+    }
     int sum = 0;
     stringstream ss(numbers);
     string num, strg;
     while (getline(ss, strg, '\n')) {
         stringstream lineStream(strg);
-        while (getline(lineStream, num, ',')) {
+        while (getline(lineStream, num, delimeter)) {
             sum += stoi(num);
         }
     }
