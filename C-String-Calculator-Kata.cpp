4732a77d56ddf6f238a24c92a532fc6c372f8a5e
M04ph3u2 <116311393+M04ph3u2@users.noreply.github.com>
2023-12-03 18:37:05 +0100

    Startpoint

---
+++ C-String-Calculator-Kata.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "./add.h"
+
+using namespace std;
+
+int main(string numbers)
+{
+    return add("0");
+}
---
+++ Test-C-String-Calculator-Kata/Test-C-String-Calculator-Kata.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../add.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestCStringCalculatorKata
+{
+	TEST_CLASS(TestCStringCalculatorKata)
+	{
+	public:
+
+	};
+}
